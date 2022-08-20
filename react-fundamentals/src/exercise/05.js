4118df6f2344793b45f82a39b1d71dd65d14c875
CCherry07 <2405693142@qq.com>
2022-08-20 21:21:21 +0800

    chore:  size class name

--- react-fundamentals/src/exercise/05.js
+++ react-fundamentals/src/exercise/05.js
@@ -6,8 +6,8 @@ import '../box-styles.css'
 
 function Box(props) {
   const { className = "" ,size="medium",style,...otherProps} = props
-  const ownClassName = `box--${size}`
-  return <div className={`box ${ownClassName}`} style={{ fontStyle:"italic" , ...style }} {...otherProps} />
+  const sizeClassName = `box--${size}`
+  return <div className={`box ${className} ${sizeClassName}`} style={{ fontStyle:"italic" , ...style }} {...otherProps} />
 }
 const smallBox = <Box size="small" style={{backgroundColor:"lightblue"}}>small lightblue box</Box>
 const mediumBox = <Box size="medium" style={{backgroundColor:"pink"}}>medium pink box</Box>
