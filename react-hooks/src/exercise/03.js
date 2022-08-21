1647721fd0dd7dbbc15e458044e7b9dfdb45efdd
CCherry07 <2405693142@qq.com>
2022-08-21 18:46:04 +0800

    feat:  colocating state

--- react-hooks/src/exercise/03.js
+++ react-hooks/src/exercise/03.js
@@ -3,11 +3,12 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={event=>setName(event.target.value)} />
     </div>
   )
 }
@@ -25,18 +26,21 @@ function FavoriteAnimal({animal,setAnimal}) {
   )
 }
 
-function Display({name,animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+// function Display({name,animal}) {
+//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+// }
+
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       <FavoriteAnimal animal={animal} setAnimal={setAnimal} />
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
