e7e1015be1824da3327d99565eb60db590080adb
CCherry07 <2405693142@qq.com>
2022-08-21 22:00:05 +0800

    chore:  change all state to lift

--- react-hooks/src/exercise/04.js
+++ react-hooks/src/exercise/04.js
@@ -4,45 +4,19 @@
 import * as React from 'react'
 
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
   const [squares,setSquares] = React.useState(Array(9).fill(null))
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
+  const winner = calculateWinner(squares)
+  const nextValue = calculateNextValue(squares)
+  const status = calculateStatus(winner,squares,nextValue)
   function selectSquare(square) {
-    if (squares[square]) 
-      throw new Error("like someone clicked a square that's already been clicked")
-      
-    if (calculateWinner(squares)) return
+    if (squares[square] || winner) return
     const newsquares = [...squares]
-    newsquares[square] = calculateNextValue(squares)
+    newsquares[square] = nextValue
     setSquares(newsquares)
-    
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
   }
 
   function restart() {
     setSquares(Array(9).fill(null))
-    // 💰 `Array(9).fill(null)` will do it!
   }
 
   function renderSquare(i) {
@@ -55,7 +29,7 @@ function Board() {
 
   return (
     <div>
-      <div className="status">{calculateStatus(calculateWinner(squares),squares,calculateNextValue(squares))}</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
