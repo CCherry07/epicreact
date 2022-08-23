17f2728ef6874f5d70cb9a835dd3259bf4943d73
CCherry07 <2405693142@qq.com>
2022-08-23 22:19:59 +0800

    feat:  traditional dispatch object with a type

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,12 +3,38 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  const countReducer = (state,action)=>({...state , ...(typeof action === "function" ? action(state) : action)})
-  const [state, changeState] = React.useReducer(countReducer, {count:initialCount})
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + action.step }
+    case "DECREMENT":
+      return { ...state, count: state.count - action.step }
+    default:
+      { throw new Error(`Unsupported action type ${action.type}`) }
+  }
+}
+
+function Counter({ initialCount = 0, step = 1 }) {
+  // const countReducer = (state,action)=>({...state , ...(typeof action === "function" ? action(state) : action)})
+  // const [state, changeState] = React.useReducer(countReducer, {count:initialCount})
+  // const { count } = state
+  // const increment = () => changeState((currentState=>({count:currentState.count + step})))
+
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
   const { count } = state
-  const increment = () => changeState((currentState=>({count:currentState.count + step})))
-  return <button onClick={increment}>{count}</button>
+  const increment = () => dispatch({ type: "INCREMENT", step })
+  const decrement = () => dispatch({ type: "DECREMENT", step })
+
+  const countAction = (type)=>()=>dispatch({type,step})
+  return (
+    <div>
+      <h1> {count} </h1>
+      <button onClick={increment}> SUM </button>
+      <button onClick={countAction("DECREMENT")}> SUB </button>
+    </div>
+  )
 }
 
 function App() {
