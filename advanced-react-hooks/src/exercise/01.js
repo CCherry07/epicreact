0cb6cc524034ee5d6c96d10fe12a5e62f8e514d7
CCherry07 <2405693142@qq.com>
2022-08-23 21:58:46 +0800

    feat: simulate setState with an object OR function

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -4,12 +4,10 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
+  const countReducer = (state,action)=>({...state , ...(typeof action === "function" ? action(state) : action)})
   const [state, changeState] = React.useReducer(countReducer, {count:initialCount})
   const { count } = state
-  function countReducer(initialCount,action) {
-    return { ...initialCount , ...action }
-  }
-  const increment = () => changeState({ count: count + step })
+  const increment = () => changeState((currentState=>({count:currentState.count + step})))
   return <button onClick={increment}>{count}</button>
 }
 
