fb857ef5af1dbbf50909842da315c2866c3ae563
CCherry07 <2405693142@qq.com>
2022-08-24 16:21:36 +0800

    feat:  `useCount`

--- advanced-react-hooks/src/exercise/03.js
+++ advanced-react-hooks/src/exercise/03.js
@@ -8,14 +8,22 @@ const CountProvider = function ({ children }) {
   const [count, setCount] = React.useState(0)
   return <CountContext.Provider value={{ count, setCount }}> {children} </CountContext.Provider>
 }
+CountContext.displayName = "CountContext"
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error(`${CountContext.displayName} must be used within then CountProvider`)
+  }
+  return context
+}
 
 function CountDisplay() {
-  const { count } = React.useContext(CountContext)
+  const { count } = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const { setCount } = React.useContext(CountContext)
+  const { setCount } = useCount()
   const increment = () => setCount(count => count + 1)
   return <button onClick={increment}>Increment count</button>
 }
