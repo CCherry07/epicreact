610ff0078685743e181639e2e64999d6227c8b64
CCherry07 <2405693142@qq.com>
2022-08-28 19:07:35 +0800

    feat:  conext memoize with React.useMemo

--- react-performance/src/exercise/05.js
+++ react-performance/src/exercise/05.js
@@ -39,7 +39,7 @@ function AppProvider({children}) {
     grid: initialGrid,
   })
   // 🐨 memoize this value with React.useMemo
-  const value = [state, dispatch]
+  const value = React.useMemo(()=>[state, dispatch] , [state , dispatch])
   return (
     <AppStateContext.Provider value={value}>
       {children}
