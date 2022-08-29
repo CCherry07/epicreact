0ed524682cf1357f47470d1ba632c52c4c58b7ab
CCherry07 <2405693142@qq.com>
2022-08-29 12:33:04 +0800

    feat:  recoil

--- react-performance/src/exercise/06.extra-4.js
+++ react-performance/src/exercise/06.extra-4.js
@@ -11,14 +11,19 @@ import {
   updateGridCellState,
 } from '../utils'
 // 🐨 you're gonna need these:
-// import {RecoilRoot, useRecoilState, useRecoilCallback, atomFamily} from 'recoil'
+import { RecoilRoot, useRecoilState, useRecoilCallback, atomFamily } from 'recoil'
 
 const AppStateContext = React.createContext()
 
-const initialGrid = Array.from({length: 100}, () =>
-  Array.from({length: 100}, () => Math.random() * 100),
+const initialGrid = Array.from({ length: 100 }, () =>
+  Array.from({ length: 100 }, () => Math.random() * 100),
 )
 
+
+const cellAtoms = atomFamily({
+  key: 'cells',
+  default: ({ row, column }) => initialGrid[row][column],
+})
 // 🐨 create an atomFamily called `cellAtoms` here where the
 // default callback function accepts an object with the
 // `row` and `column` and returns the value from the initialGrid
@@ -29,30 +34,22 @@ const initialGrid = Array.from({length: 100}, () =>
 // Here's how it's used:
 // const updateGrid = useUpdateGrid()
 // then later: updateGrid({rows, columns})
-// function useUpdateGrid() {
-//   return useRecoilCallback(({set}) => ({rows, columns}) => {
-//     for (let row = 0; row < rows; row++) {
-//       for (let column = 0; column < columns; column++) {
-//         if (Math.random() > 0.7) {
-//           set(cellAtoms({row, column}), Math.random() * 100)
-//         }
-//       }
-//     }
-//   })
-// }
+function useUpdateGrid() {
+  return useRecoilCallback(({ set }) => ({ rows, columns }) => {
+    for (let row = 0; row < rows; row++) {
+      for (let column = 0; column < columns; column++) {
+        if (Math.random() > 0.7) {
+          set(cellAtoms({ row, column }), Math.random() * 100)
+        }
+      }
+    }
+  })
+}
 
 function appReducer(state, action) {
   switch (action.type) {
     case 'TYPED_IN_DOG_INPUT': {
-      return {...state, dogName: action.dogName}
-    }
-    // 💣 we're going to use recoil to update the cell values, so delete this case
-    case 'UPDATE_GRID_CELL': {
-      return {...state, grid: updateGridCellState(state.grid, action)}
-    }
-    // 💣 the useUpdateGrid hook above will handle this. Delete this case.
-    case 'UPDATE_GRID': {
-      return {...state, grid: updateGridState(state.grid)}
+      return { ...state, dogName: action.dogName }
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -60,11 +57,9 @@ function appReducer(state, action) {
   }
 }
 
-function AppProvider({children}) {
+function AppProvider({ children }) {
   const [state, dispatch] = React.useReducer(appReducer, {
     dogName: '',
-    // 💣 we're moving our state outside of React with our atom, delete this:
-    grid: initialGrid,
   })
   // 🦉 notice that we don't even need to bother memoizing this value
   const value = [state, dispatch]
@@ -85,11 +80,11 @@ function useAppState() {
 
 function Grid() {
   // 🐨 we're no longer storing the grid in our app state, so instead you
+  const updateGrid = useUpdateGrid()
   // want to get the updateGrid function from useUpdateGrid
-  const [, dispatch] = useAppState()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
-  const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
+  const updateGridData = () => updateGrid({ rows, columns })
   return (
     <AppGrid
       onUpdateGrid={updateGridData}
@@ -101,17 +96,10 @@ function Grid() {
     />
   )
 }
-// 💣 remove memoization. It's not needed!
-Grid = React.memo(Grid)
 
-function Cell({row, column}) {
-  // 🐨 replace these three lines with useRecoilState for the cellAtoms
-  // 💰 Here's how you calculate the new value for the cell when it's clicked:
-  //    Math.random() * 100
-  const [state, dispatch] = useAppState()
-  const cell = state.grid[row][column]
-  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
-
+function Cell({ row, column }) {
+  const [cell, setCell] = useRecoilState(cellAtoms({ row, column }))
+  const handleClick = () => setCell(Math.random() * 100)
   return (
     <button
       className="cell"
@@ -125,17 +113,14 @@ function Cell({row, column}) {
     </button>
   )
 }
-// 🦉 notice we don't need to bother memoizing any of the components!!
-// 💣 remove memoization
-Cell = React.memo(Cell)
 
 function DogNameInput() {
   const [state, dispatch] = useAppState()
-  const {dogName} = state
+  const { dogName } = state
 
   function handleChange(event) {
     const newDogName = event.target.value
-    dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
+    dispatch({ type: 'TYPED_IN_DOG_INPUT', dogName: newDogName })
   }
 
   return (
@@ -160,13 +145,14 @@ function App() {
   return (
     <div className="grid-app">
       <button onClick={forceRerender}>force rerender</button>
-      {/* 🐨 wrap this in a RecoilRoot */}
-      <AppProvider>
-        <div>
-          <DogNameInput />
-          <Grid />
-        </div>
-      </AppProvider>
+      <RecoilRoot>
+        <AppProvider>
+          <div>
+            <DogNameInput />
+            <Grid />
+          </div>
+        </AppProvider>
+      </RecoilRoot>
     </div>
   )
 }
