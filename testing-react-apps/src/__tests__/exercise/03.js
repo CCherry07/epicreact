0241b0a98b637fdc081e1e22a758ec922539f03e
CCherry07 <2405693142@qq.com>
2022-08-30 19:54:16 +0800

    feat:  use userEvent trigger event

--- testing-react-apps/src/__tests__/exercise/03.js
+++ testing-react-apps/src/__tests__/exercise/03.js
@@ -3,20 +3,19 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, screen , fireEvent} from '@testing-library/react'
+import {render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
   render(<Counter />)
-  // 🐨 replace these with screen queries
   const decrement = screen.getByRole("button" , { name:/decrement/i })
   const increment = screen.getByRole("button" , { name:/increment/i })
-
   const message = screen.getByText(/Current count/)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
