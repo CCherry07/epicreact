2268e06ef72af5035b87d5ee0fd3e09e380cda7e
CCherry07 <2405693142@qq.com>
2022-09-01 15:52:41 +0800

    test:  custom hook initialProps

--- testing-react-apps/src/__tests__/exercise/08.js
+++ testing-react-apps/src/__tests__/exercise/08.js
@@ -2,24 +2,42 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import { render, screen ,act } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen, act } from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-
-test('exposes the count and increment/decrement functions', async () => {
-  let result
-  function TestComponent(params) {
-    result = useCounter()
+function renderHook({ initialProps } = {}) {
+  const result = {}
+  function TestComponent() {
+    result.current = useCounter(initialProps)
     return null
   }
+  render(<TestComponent />)
+  return result
+}
 
-  render(<TestComponent/>)
-  expect(result.count).toBe(0)
-  act(()=>result.increment())
-  expect(result.count).toBe(1)
-  act(()=>result.decrement())
-  expect(result.count).toBe(0)
+test('exposes the count and increment/decrement functions', async () => {
+  const result = renderHook()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+test('allows customization of the initial count', async () => {
+  const result = renderHook({ initialProps: { initialCount: 3 } })
+  expect(result.current.count).toBe(3)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(4)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(3)
+})
+test('allows customization of the step', async () => {
+  const result = renderHook({ initialProps: { step: 2 } })
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
