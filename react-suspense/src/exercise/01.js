55bf7fbdb6a40ffd9a7a13702677404175cf6a31
CCherry07 <2405693142@qq.com>
2022-09-02 21:58:30 +0800

    feat:  error Boundary

--- react-suspense/src/exercise/01.js
+++ react-suspense/src/exercise/01.js
@@ -2,14 +2,20 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import { PokemonDataView, fetchPokemon } from '../pokemon'
+import { PokemonDataView, fetchPokemon, PokemonErrorBoundary } from '../pokemon'
 
 let pokemon
-const pokemonPromise = fetchPokemon("pikachu").then((pokemonData) => {
+let pokemonError
+const pokemonPromise = fetchPokemon("pikach").then((pokemonData) => {
   pokemon = pokemonData
+}, (error) => {
+  pokemonError = error
 })
 
 function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError
+  }
   if (!pokemon) {
     throw pokemonPromise
   }
@@ -27,9 +33,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={<div>loading...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>loading...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
